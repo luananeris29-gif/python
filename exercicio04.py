1ffe840516bc3680e543e949aa8393f775499c2f
Luana Neris <luananeris29@gmail.com>
2026-08-24 15:00:09 -0300

    exercicio04.py

---
+++ exercicio04.py
@@ -0,0 +1,7 @@
+digiteumvalor= float(input("digite um valor:"))
+dobro = digiteumvalor * 2
+triplo = digiteumvalor * 3
+metade = digiteumvalor / 2
+print (f"o dobro do valor digitado é: {dobro}")
+print (f"o triplo do valor digitado é: {triplo}")
+print (f"a metade do valor digitado é: {metade}")   

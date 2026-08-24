0dea4c4d4471f621f9273872700323d1524636b7
Luana Neris <luananeris29@gmail.com>
2026-08-24 15:33:32 -0300

    exercicio08.py

---
+++ exercicio08.py
@@ -0,0 +1,5 @@
+preco= float(input("digite o preço do produto:"))
+desconto= preco*0.1
+precofinal= preco-desconto  
+print (f"o preço do produto com desconto é: {desconto}")
+print (f"o preço final do produto é: {precofinal}")

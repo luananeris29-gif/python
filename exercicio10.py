ba24356d96504acf8b98d8f7a0d9556984be03e9
Luana Neris <luananeris29@gmail.com>
2026-08-24 15:45:23 -0300

    exercicio10.py

---
+++ exercicio10.py
@@ -0,0 +1,6 @@
+salariofixo= float(input("digite o salario fixo:"))
+totalvendido= float(input("digite o total vendido:"))
+comissao= totalvendido*0.04
+salariototal= salariofixo + comissao
+print (f"o valor da comissão é: {comissao}")
+print (f"o salario total do vendedor é: {salariototal}")

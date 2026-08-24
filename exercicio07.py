541e3013efcd4f24834e0d2e13d670bf1a6ca15d
Luana Neris <luananeris29@gmail.com>
2026-08-24 15:19:47 -0300

    exercicio07.py

---
+++ exercicio07.py
@@ -0,0 +1,3 @@
+temperaturaemcelsius= float(input("digite a temperatura em celsius:"))
+temperaturaemfahrenheit= (temperaturaemcelsius * 9/5) + 32
+print (f"a temperatura digitada em fahrenheit é: {temperaturaemfahrenheit}")    

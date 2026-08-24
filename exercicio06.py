749f2e5fc128ff6c2652b1f10a7bcef18c2ba25c
Luana Neris <luananeris29@gmail.com>
2026-08-24 15:17:36 -0300

    exercicio06.py

---
+++ exercicio06.py
@@ -0,0 +1,6 @@
+largura= float(input("digite a largura:"))
+altura= float(input("digite a altura:"))
+area= largura * altura
+perimetro= (largura + altura) * 2
+print (f"a área do retângulo é: {area}")
+print (f"o perímetro do retângulo é: {perimetro}")

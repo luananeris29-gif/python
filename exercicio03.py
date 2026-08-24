a04ca3e54404c69f2a8f6823025a04c20162f931
Luana Neris <luananeris29@gmail.com>
2026-08-24 14:52:06 -0300

    exercicio03.py

---
+++ exercicio03.py
@@ -0,0 +1,5 @@
+digiteumnumero = int(input("digite um número:"))
+antecessor = digiteumnumero - 1
+sucessor = digiteumnumero + 1
+print (f"o antecessor do número digitado é: {antecessor}")
+print (f"o sucessor do número digitado é: {sucessor}")

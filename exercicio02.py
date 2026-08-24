955df859d4f9e29aeb5585262b77e4d4fb567603
Luana Neris <luananeris29@gmail.com>
2026-08-24 11:03:26 -0300

    exercicio02.py

---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+nota1 = input("primeira nota:") 
+nota2 = input ("segunda nota:")
+media= (float(nota1)+float(nota2))/2 
+print (f"a média das notas digitas é: {media:1f}")
+#float significa número decimal, convertendo o valor digitado pelo usuário em um número decimal.
+#normalmente utilizado para notas, pois tem casas decimais.
+

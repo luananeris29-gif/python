96374b65edbbde7e9b29b6638201224c713e2da6
Luana Neris <luananeris29@gmail.com>
2026-08-24 15:11:42 -0300

    exercicio05.py

---
+++ exercicio05.py
@@ -0,0 +1,5 @@
+metro=  float (input("digite um valor em metros:"))
+centrimetro= metro * 100
+milimetro= metro * 1000
+print (f"o valor digitado em centímetros é: {centrimetro}cm")
+print (f"o valor digitado em milímetros é: {milimetro}mm")

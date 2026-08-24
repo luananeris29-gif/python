738ea12be4647dfeb9621ef515624696695bab69
Luana Neris <luananeris29@gmail.com>
2026-08-24 15:39:58 -0300

    exercicios09.py

---
+++ exercicio09.py
@@ -0,0 +1,5 @@
+salarioatural= float(input("digite o salario atual:"))
+aumento= salarioatural*0.15
+novosalario= salarioatural+aumento
+print (f"o aumento do salario é: {aumento}")
+print (f"o novo salario é: {novosalario}")  

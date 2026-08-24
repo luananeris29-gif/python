eb8bb4e39c65285ae63caadf7fb5911ca6b93b46
Luana Neris <luananeris29@gmail.com>
2026-08-23 23:11:54 -0300

    exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+primeiroValor= input ("digite o primeiro valor:")
+segundoValor= input ("digite o segundo valor:")
+soma= float(primeiroValor)+float(segundoValor)
+media= soma/2
+#int significa número inteiro, convertendo o valor digitado pelo usuário em um número inteiro.
+#float significa número de ponto flutuante, convertendo o valor digitado pelo usuário em um número de ponto flutuante.
+print (f"A média dos valores digitados é: {media}")

a98d9354e17baf5602225f06c3d076c0f554e316
Luana Neris <luananeris29@gmail.com>
2026-08-25 20:38:46 -0300

    exercicio14.py
    conforme orientado em aula, a apostila não havia os seguintes exercícios: 11, 12, e 13, sendo assim, orientado a pular para o 14º exercício.

---
+++ exercicio14.py
@@ -0,0 +1,7 @@
+a= int(input("digite a variavel a:"))
+b= int(input("digite a variavel b:"))
+aux= a
+a= b
+b= aux
+print (f"depois da troca a variavel a é: {a}")
+print (f"depois da troca a variavel b é: {b}")

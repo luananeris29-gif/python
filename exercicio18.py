88c1174598c212596f2567ecd5abc33781c8d8ca
Luana Neris <luananeris29@gmail.com>
2026-09-16 23:09:42 -0300

    exercicio18.py

---
+++ exercicio18.py
@@ -0,0 +1,9 @@
+numero1 = float(input("primeiro valor"))
+numero2 = float(input("segundo valor:"))
+
+if numero1> numero2:
+    print ("maior valor:",numero1)
+elif numero2 > numero1:
+    print ("maior valor:",numero2)
+else:
+    print ("valores iguais.") 

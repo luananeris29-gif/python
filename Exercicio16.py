45dddd6d3b019a70548b39139e68adfd1b99e91f
Luana Neris <luananeris29@gmail.com>
2026-09-17 01:41:57 -0300

    Delete Exercicio16.py

--- Exercicio16.py
+++
@@ -1,8 +0,0 @@
-numero = float(input("Digite um número: "))
-if numero >  0:
-    print("O número é positivo.")   
-elif numero < 0:
-    print("O número é negativo.")
-else:
-    print("O número é zero")   
-   

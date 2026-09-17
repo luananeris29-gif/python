f156e0d2d2c76240b8372573e78f5776d344de75
Luana Neris <luananeris29@gmail.com>
2026-09-17 01:43:20 -0300

    exercicio21.py

---
+++ exercicio21.py
@@ -0,0 +1,10 @@
+nota1 = float(input("digite a nota 1:"))
+nota2= float(input("digite a nota 2:"))
+media = (nota1 + nota2) /2
+print ("media:", media)
+
+if media >= 7: 
+    print ("aprovado")
+else:
+    print ("reprovado")
+

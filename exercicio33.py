a9cd8d57b557228b42b9766acea72cfb62f8de32
Luana Neris <luananeris29@gmail.com>
2026-09-21 09:48:34 -0300

    exercicio33.py

---
+++ exercicio33.py
@@ -0,0 +1,18 @@
+n = int(input("Número (1 a 7): "))
+
+if n == 1:
+    print("SEGUNDA-FEIRA")
+elif n == 2:
+    print("TERÇA-FEIRA")
+elif n == 3:
+    print("QUARTA-FEIRA")
+elif n == 4:
+    print("QUINTA-FEIRA")
+elif n == 5:
+    print("SEXTA-FEIRA")
+elif n == 6:
+    print("SÁBADO")
+elif n == 7:
+    print("DOMINGO")
+else:
+    print("OPÇÃO INVÁLIDA")

e98ebcec0115c7417390c306dd44519834d74820
Luana Neris <luananeris29@gmail.com>
2026-09-21 08:57:09 -0300

    exercicio23.py

---
+++ exercicio23.py
@@ -0,0 +1,10 @@
+idade= int(input("digita sua idade : "))
+
+if idade <= 16:
+  print ("não pode votar")
+elif idade <= 17:
+   print ("voto opcional")
+elif idade <= 69:
+   print ("voto obrigatorio")
+else:
+   print ("voto opcional")
